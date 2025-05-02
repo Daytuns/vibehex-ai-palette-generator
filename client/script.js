745e3dfcef62abc9adb02c6fbbcb6f6cc637a3d9
Daytuns <daytonbaldizon@gmail.com>
2025-05-02 05:04:02 -0600

    few frontend styling changes

--- client/script.js
+++ client/script.js
@@ -1,3 +1,12 @@
+
+const firstDiv = document.getElementById('one');
+const secondDiv = document.getElementById('two');
+const thirdDiv = document.getElementById('three');
+const fourthDiv = document.getElementById('four');
+const fifthDiv = document.getElementById('five');
+const divs = [firstDiv, secondDiv, thirdDiv, fourthDiv, fifthDiv];
+
+
 document.querySelector("form").addEventListener("submit", async (e) => {
     e.preventDefault();
 
@@ -32,6 +41,7 @@ document.querySelector("form").addEventListener("submit", async (e) => {
         // Display the palette
         const colorsDiv = document.createElement("div");
         colorsDiv.style.display = "flex";
+        colorsDiv.id = "colorsDiv";
         palette.forEach(color => {
             const swatch = document.createElement("div");
             swatch.style.backgroundColor = color;
@@ -41,7 +51,14 @@ document.querySelector("form").addEventListener("submit", async (e) => {
             colorsDiv.appendChild(swatch);
         });
         document.body.appendChild(colorsDiv);
+        for (let i=0; i<5;i++){
+            document.getElementById(`${divs[i].id}`).style.backgroundColor = palette[i];
+            document.getElementById(`${divs[i].id}Head`).textContent = palette[i];
+        }
     } else {
         console.error(data.error);
     }
+
 });
+
+
